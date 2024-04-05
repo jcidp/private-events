642620ebc870e2782a7a9af36e8a8f99348fb231
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-04-04 22:35:21 -0600

    User can create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,22 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :date, :location, :private)
+  end
 end
