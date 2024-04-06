763a2b915f94479f73bb3ecc3e58705b90b5faa6
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-04-06 09:24:52 -0600

    Require login to use app

--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -1,4 +1,6 @@
 class AttendanceController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @event = Event.find(params[:event_id])
     if @event.attendees << current_user
