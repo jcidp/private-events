04c98b97681fbc4025ce58a400877c1ae478b71c
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-04-05 20:33:26 -0600

    Add past & upcoming event scopes to the Index view

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,15 @@ class Event < ApplicationRecord
 
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   has_and_belongs_to_many :attendees, class_name: "User", join_table: "attendance"
+
+  scope :past, -> { where(date: ..Time.now) }
+  scope :upcoming, -> { where(date: Time.now...) }
+
+  # def self.past
+  #   self.where(date: ..Time.now)
+  # end
+
+  # def self.upcoming
+  #   self.where(date: Time.now...)
+  # end
 end
