b8c3307e84ab8b442197e0621e72e325571bfbd5
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-04-05 22:29:41 -0600

    Users can edit & delete events they created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,10 +14,29 @@ class EventsController < ApplicationController
   def create
     @event = current_user.events.build(event_params)
     if @event.save
-      redirect_to users_path
+      redirect_to @event
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to users_path
   end
 
   private
